8c242cffb337f71819ae78e2ba22549a9827a276
HuanLD <huanld@HuanLDs-Mac-mini.local>
2019-02-15 09:22:07 +0700

    activity lifecycler

--- app/src/main/java/com/example/intent_explicit/Main2Activity.java
+++ app/src/main/java/com/example/intent_explicit/Main2Activity.java
@@ -1,12 +1,14 @@
 package com.example.intent_explicit;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends Activity {
 
     Button btnSecond;
 
@@ -24,5 +26,54 @@ public class Main2Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Log.d("AAA","onCreate Main2");
+
+    }
+
+    public Main2Activity() {
+        super();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("AAA","onStart Main2");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("AAA","onRestart Main2");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("AAA","onResume Main2");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("AAA","onPause Main2");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("AAA","onStop Main2");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("AAA","onDestroy Main2");
+
     }
 }
--- app/src/main/java/com/example/intent_explicit/MainActivity.java
+++ app/src/main/java/com/example/intent_explicit/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.intent_explicit;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     Button btnMain;
 
@@ -24,5 +26,48 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        Log.d("AAA","onCreate Main1");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("AAA","onStart Main1");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("AAA","onRestart Main1");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("AAA","onResume Main1");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("AAA","onPause Main1");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("AAA","onStop Main1");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("AAA","onDestroy Main1");
+
     }
 }
